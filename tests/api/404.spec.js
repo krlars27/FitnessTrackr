b682c2966f3dbae59e07af6e5cb77b69dbdc7d6e
Bart Dorsey <bart@bartdorsey.com>
2021-11-16 13:09:50 -0600

    adding comment on 404 test

--- tests/api/404.spec.js
+++ tests/api/404.spec.js
@@ -12,6 +12,7 @@ describe("/api/unknown", () => {
   it("should return a 404", async (done) => {
     const response = await request(app).get("/api/unknown");
     expect(response.status).toEqual(404);
+    // the 404 response returns an object with a message property
     expect(typeof response.body.message).toEqual("string");
     done();
   });
