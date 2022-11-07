3a0ab2a7168464adc9438a57834d6ec3e76f0756
Maaya <yamauchimaaya@gmail.com>
2022-11-07 10:29:39 -0600

    The Methods

--- db/seedData.js
+++ db/seedData.js
@@ -3,19 +3,62 @@ const { createUser, createActivity, createRoutine, getRoutinesWithoutActivities 
 const client = require("./client")
 
 async function dropTables() {
-  console.log("Dropping All Tables...");
-  // drop all tables, in the correct order
-  await client.query(`DROP TABLE IF EXISTS mytablename;`)
+    // drop all tables, in the correct order
+
+  try {
+    console.log("Dropping All Tables...");
+
+    await client.query(`DROP TABLE IF EXISTS mytablename;`)
+
+  } catch (err) {
+    console.error(err.message)
+  }
   
 }
 
 async function createTables() {
-  console.log("Starting to build tables...");
-  // create all tables, in the correct order
-  await client.query (`CREATE TABLE mytablename(
+  try {
+    console.log("Starting to build tables...");
+    // create all tables, in the correct order
 
-    id SERIAL PRIMARY KEY
-  );`);
+    // const lowercase=text.toLowerCase(); use when writing out server
+
+
+    await client.query (
+
+      `CREATE TABLE users(
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(255) UNIQUE NOT NULL, 
+        password VARCHAR(255) NOT NULL
+        );
+        CREATE TABLE activity(
+          id SERIAL PRIMARY KEY, 
+          name VARCHAR(255) UNIQUE NOT NULL,
+          description TEXT NOT NULL
+        );
+        CREATE TABLE routines(
+          id SERIAL PRIMARY KEY, 
+          "creatorId" INTEGER REFERENCES users(id),
+          "isPublic" BOOLEAN DEFAULT false,
+          name VARCHAR(255) UNIQUE NOT NULL,
+          goal TEXT NOT NULL
+        );
+        CREATE TABLE routine_activities(
+          id SERIAL PRIMARY KEY, 
+          "routineId" INTEGER UNIQUE REFERENCES routines (id),
+          "activityId" INTEGER UNIQUE REFERENCES activities(id),
+          duration INTEGER,
+          count INTEGER
+        );
+        `
+        
+    );
+
+  } catch (err) {
+    console.error(err.message)
+  }
+
+  
 }
 
 /* 
