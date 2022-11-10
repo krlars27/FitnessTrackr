46630bcc3ab7bc747472bfc6cb4fff9170b055e7
ADMIN <admin@ADMINs-Mac-mini.local>
2022-11-10 14:25:18 -0600

    working

--- api/routines.js
+++ api/routines.js
@@ -1,46 +1,37 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { getAllPublicRoutines, createRoutine } = require('../db/routines')
+const { getAllPublicRoutines, createRoutine } = require("../db/routines");
 const jwt = require("jsonwebtoken");
 // GET /api/routines
-router.get('/', async (req, res, next) => {
-    try {
-      const allRoutines = await getAllPublicRoutines();
-    
-      
+router.get("/", async (req, res, next) => {
+  try {
+    const allRoutines = await getAllPublicRoutines();
 
-      res.send(
-        allRoutines
-      );
-        
-    } catch (error) {
-        next (error);
-    }
-    
+    res.send(allRoutines);
+  } catch (error) {
+    next(error);
+  }
 });
 
 // POST /api/routines
-router.post('/', async (req, res, next)=> {
-    const { creatorId, isPublic, name, goal } = req.body
-   
-    try
-     {
-        const newRoutine = await createRoutine({ creatorId, isPublic, name, goal });
-        if (newRoutine){
-        res.send(
-            newRoutine
-        )} else { console.log(newRoutine)
-            next(
-                {name:'duplicate name',
-            message:'You must be logged in to perform this action'}
-            )
-        }
-        
-    } catch ({message, name}) {
-        next({message, name})
-        
+router.post("/", async (req, res, next) => {
+  const { creatorId, isPublic, name, goal } = req.body;
+console.log(creatorId, 'please work')
+  try {
+    const newRoutine = await createRoutine({ creatorId, isPublic, name, goal });
+    if (newRoutine) {
+      res.send(newRoutine);
+    } else {
+      console.log(newRoutine, 'hoops');
+      next({
+        name: "duplicate name",
+        message: "You must be logged in to perform this action",
+      });
     }
-})
+  } catch ({ message, name }) {
+    next({ message, name });
+  }
+});
 
 // PATCH /api/routines/:routineId
 
