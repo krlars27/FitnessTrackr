9b1f9076477fda4192284384e2532695773bfa69
Maaya <yamauchimaaya@gmail.com>
2022-11-14 12:43:48 -0600

    finished backend. Installed create-react-app

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React, {Fragment} from 'react'
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+  
+} from "react-router-dom"; 
+
+function App() {
+  return (
+    <Fragment>
+      <Router>
+        <div className="container">
+          <Routes>
+            <Route>
+      
+            </Route>
+          </Routes>
+        </div>
+      </Router>
+    </Fragment>
+  );
+}
+
+export default App;
